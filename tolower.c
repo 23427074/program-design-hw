0f7d00e61583c5876609e42095ec67d94bc41491
23427074 <77039165+23427074@users.noreply.github.com>
2021-06-23 18:59:02 +0800

    Update tolower.c

--- tolower.c
+++ tolower.c
@@ -7,11 +7,18 @@ int main(int argc, char **argv)
     FILE *file;
     if(argc==3)
     {
-        file = fopen(argv[2], "r+");
+        file = fopen(argv[2], "r");
     }
-    char line[1024];
+    char *line = malloc(sizeof(char)*1024);
+    char **word = malloc(sizeof(char*)*1024);
+    for(int i=0;i<1024;i++)
+    {
+        *(word+i) = malloc(sizeof(char)*1024);
+    }
+    int j=0;
     while(fgets(line, 1024, file)!=NULL)
     {
+        line[strcspn(line,"\n")]='\0';
         int len = strlen(line);
         for(int i=0;i<len;i++)
         {
@@ -20,7 +27,13 @@ int main(int argc, char **argv)
                 line[i]+=32;
             }
         }
-        printf("%s\n",line);
+        strcpy(word[j++],line);
+    }
+    fclose(file);
+    file = fopen(argv[2],"w+");
+    for(int y=0;y<j;y++)
+    {
+        fprintf(file, "%s\n",word[y]);
     }
     fclose(file);
 }
